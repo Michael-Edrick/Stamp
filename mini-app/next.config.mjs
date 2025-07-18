55ada93d1975d4eccc0df5786aec19b3a43c01a8
unknown <michael.edrick@gmail.com>
2025-07-18 17:23:46 +0700

    feat: add image remote pattern for pravatar

--- mini-app/next.config.mjs
+++ mini-app/next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
