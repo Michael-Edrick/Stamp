e052780247854982a3b6914d3cbf980c9d1ba009
unknown <michael.edrick@gmail.com>
2025-09-26 18:02:02 +0700

    update metadata on layout

--- mini-app/app/layout.tsx
+++ mini-app/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter, Roboto, Chivo_Mono } from "next/font/google";
 import "./globals.css";
 import "./theme.css";
 import "@coinbase/onchainkit/styles.css";
+import type { Metadata, Viewport } from "next";
 import { Providers } from "./providers";
 import { Toaster } from 'sonner';
 import AppFrame from "./AppFrame";
@@ -23,31 +24,45 @@ const chivo_mono = Chivo_Mono({
   variable: '--font-chivo-mono',
 });
 
-const appUrl = process.env.NEXT_PUBLIC_URL || 'http://localhost:3000';
-const appName = process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'StampMe';
-const heroUrl = `${appUrl}/hero.png`;
-const postUrl = `${appUrl}/api/farcaster-signin`;
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL || 'https://stamp-me.vercel.app';
+  const appName = process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'StampMe';
+
+  return {
+    title: appName,
+    description: "A simple app to reach anyone.",
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+        imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+        button: {
+          title: `Launch ${appName}`,
+          action: {
+            type: "launch_frame",
+            name: appName,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
+            splashBackgroundColor:
+              process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+          },
+        },
+      }),
+    },
+  };
+}
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="en" className="h-full">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{appName}</title>
-        <meta name="description" content="A simple app to reach anyone." />
-
-        {/* Farcaster Frame Tags for Mini App */}
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={heroUrl} />
-        <meta property="fc:frame:button:1" content={`Launch ${appName}`} />
-        <meta property="fc:frame:button:1:action" content="post" />
-        <meta property="fc:frame:post_url" content={postUrl} /> 
-      </head>
       <body className={`${inter.variable} ${roboto.variable} ${chivo_mono.variable} font-sans h-full`} style={{ backgroundColor: '#DEDEDE' }}>
         <Providers>
           <AppFrame>
