39b4aef89ba03c2159a4072d3d36e58dbf6d4355
unknown <michael.edrick@gmail.com>
2025-09-26 16:28:16 +0700

    update farcaster.json

--- mini-app/app/.well-known/farcaster.json/route.ts
+++ mini-app/app/.well-known/farcaster.json/route.ts
@@ -1,5 +1,18 @@
 
 
+function withValidProperties(
+  properties: Record<string, undefined | string | string[]>,
+) {
+  return Object.fromEntries(
+    Object.entries(properties).filter(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.length > 0;
+      }
+      return !!value;
+    }),
+  );
+}
+
 export async function GET() {
   const URL = process.env.NEXT_PUBLIC_URL || 'https://stamp-me.vercel.app';
 
@@ -9,16 +22,24 @@ export async function GET() {
       payload: "eyJkb21haW4iOiJzdGFtcC1tZS52ZXJjZWwuYXBwIn0",
       signature: "TCxSA7kk16cAaU3F1RLo/mTgeyf5PLx6ROF/HlCj0wZDdDA0TISgR4NI2EJEqHdvLx1Nhl3yrUDhaBgkqNSapRw=",
     },
-    frame: {
+    frame: withValidProperties({
       version: "1",
-      name: "StampMe",
-      imageUrl: `${URL}/hero.png`,
-      iconUrl: `${URL}/icon.png`,
+      name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+      subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
+      description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+      screenshotUrls: [],
+      iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
+      splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
+      splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
       homeUrl: URL,
       webhookUrl: `${URL}/api/webhook`,
-      splashImageUrl: `${URL}/splash.png`,
-      splashBackgroundColor: "#000000",
-      buttonTitle: "Launch StampMe"
-    },
+      primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
+      tags: [],
+      heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+      tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
+      ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
+      ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
+      ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
+    }),
   });
 }
